519b0ceeff9a3ee8bdfd859e45c21517aafddb1b
zedd45 <christopher.keen@gmail.com>
2015-06-25 10:28:29 -0700

    update app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,8 +1,20 @@
-let me = 'Chris';
-
-function doStuff() {
-    alert(me);
-}
-
-doStuff();
-
+import 'normalize.css';
+import './app.styl';
+import angular from 'angular';
+// we need to import
+// other angular modules and
+// register them him
+angular.module('app', [
+  // module dependencies here
+])
+.directive('app', ()=> {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: `
+      <div>
+        <note-maker></note-maker>
+      </div>
+    `
+  }
+})
