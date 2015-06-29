a2330d6b29855215bcc0469831021f70877dc87b
Chris Keen <christopher.keen@gmail.com>
2015-06-29 06:25:22 -0700

    move app html to it's own template.

--- client/app/components/app/app.js
+++ client/app/components/app/app.js
@@ -1,21 +1,11 @@
 import './app.styl';
+import template from './app.html';
 
 let App = () => {
   return {
     restrict: 'E',
     replace: true,
-    template: `
-      <div class="container-fluid">
-        <nav class="navbar navbar-sams navbar-fixed-top">
-          <a class="navbar-brand" href="#">
-            <img style="height: 30px" class="img-responsive" alt="Brand" src="/images/logo-large.png">
-          </a>
-        </nav>
-        <speech-recognition></speech-recognition>
-        <club-listing></club-listing>
-        <product-listing></product-listing>
-      </div>
-    `
+    template,
   }
 }
 
